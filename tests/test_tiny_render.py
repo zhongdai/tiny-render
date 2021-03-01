03c9fc68dbc2968780cc424fe416d30259dceca9
Zhong Dai <zhdai@woolworths.com.au>
2021-03-02 09:02:48 +1100

    implemented

--- tests/test_tiny_render.py
+++ tests/test_tiny_render.py
@@ -1,5 +1,14 @@
+
+import unittest
+
+from tiny_render import Render
 from tiny_render import __version__
 
+class TestTinyRender(unittest.TestCase):
+    def setUp(self):
+        pass
 
-def test_version():
-    assert __version__ == '0.1.0'
+    def test_dir_not_exist(self):
+         with self.assertRaises(ValueError):
+             _ = Render("non/exist/path")
+
--- tiny_render/render.py
+++ tiny_render/render.py
@@ -1,10 +1,46 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 import os
+import json
+import subprocess
+
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+from jinja2 import Template
 
 class Render(object):
-    def __init__(self, template_dir):
+    """A simple Jinja2 wrapper to provide the build-in variables
+    `_gitsha` - git short sha
+    `_date_str` - yyyymmdd string
+    `_time_str` - yyyymmddHHMMSS string
+    """
+    def __init__(self, template_dir: str):
+        """Init the Render by giving a directory contains your template
+        """
         if not os.path.isdir(template_dir):
             raise ValueError(f"{template_dir} is not a valid directory")
 
         self._dir = template_dir
+        self.env = Environment(loader=FileSystemLoader(template_dir))
+
+    def __repr__(self):
+        return f"Render({self._dir})"
+
+    @staticmethod
+    def get_shortsha():
+        short_hash = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+        short_hash = str(short_hash, "utf-8").strip()
+
+        return short_hash
+
+    def go(self, template: str, *args, **kwargs) -> str:
+        """ Do the actual rendering process, and print the renderred content
+        """
+        current_date = datetime.now()
+        template = self.env.get_template(template)
+        kwargs["_gitsha"] = Render.get_shortsha()
+        kwargs["_date_str"] = current_date.strftime("%Y%m%d")
+        kwargs["_time_str"] = current_date.strftime("%Y%m%d%H%M%S")
+
+        return template.render(*args, **kwargs)
